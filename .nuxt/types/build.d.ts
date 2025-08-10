507ccc470b39a249b067b75c308aa26324272c18
sinh-dev <sinh.ern.pfdev@gmail.com>
2025-08-10 23:13:36 +0700

    set up git ignore

--- .nuxt/types/build.d.ts
+++ .nuxt/types/build.d.ts
@@ -12,6 +12,8 @@ declare module "#build/paths.mjs";
 declare module "#build/root-component.mjs";
 declare module "#build/plugins.server.mjs";
 declare module "#build/test-component-wrapper.mjs";
+declare module "#build/devtools/settings.mjs";
+declare module "#build/runtime.vue-devtools-client.YE_CZoAZTZeRe0jL5zOxjSj3gSe64_kheXK3yb9yNcs.js";
 declare module "#build/routes.mjs";
 declare module "#build/pages.mjs";
 declare module "#build/router.options.mjs";
